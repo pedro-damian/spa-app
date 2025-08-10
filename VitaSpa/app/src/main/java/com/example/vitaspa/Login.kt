5d08f800cef35bd10a03be4e6b2bcf422d24b0b4
pedro-damian <p.damianoriega@gmail.com>
2025-08-10 16:56:00 -0500

    avance 2

--- VitaSpa/app/src/main/java/com/example/vitaspa/Login.kt
+++ VitaSpa/app/src/main/java/com/example/vitaspa/Login.kt
@@ -1,11 +1,31 @@
 package com.example.vitaspa
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class Login : AppCompatActivity() {
+
+    lateinit var usuario: EditText
+    lateinit var clave: EditText
+    lateinit var ingresar:Button
+    lateinit var regresar:Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        usuario = findViewById(R.id.etUsuario)
+        clave = findViewById(R.id.etClave)
+        ingresar = findViewById(R.id.btnIngresar)
+        regresar = findViewById(R.id.btnRegresar)
+
+
+        regresar.setOnClickListener {
+            val pantalla1 = Intent(this, MainActivity::class.java)
+            startActivity(pantalla1)
+            System.exit(0)
+        }
     }
 }
